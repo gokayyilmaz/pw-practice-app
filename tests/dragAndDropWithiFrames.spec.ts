f2c2927c59dac8f766a6ab65145670b47b44113b
Gokay Yilmaz <gokay6320@gmail.com>
2026-06-21 22:36:26 +0300

    drag and drop - iframe tc added

---
+++ tests/dragAndDropWithiFrames.spec.ts
@@ -0,0 +1,18 @@
+import {test,expect} from "@playwright/test"
+
+test("drag and drop with iframe", async({page}) => {
+  await page.goto("https://www.globalsqa.com/demo-site/draganddrop/")
+  const frame = page.frameLocator("[rel-title='Photo Manager'] iframe")
+
+  // with dragTo()
+  await frame.getByText("High Tatras 2").dragTo(frame.locator("#trash"))
+
+  // with mouse control
+  await frame.getByText("High Tatras 4").hover()
+  await page.mouse.down()
+  await frame.locator("#trash").hover()
+  await page.mouse.up()
+
+  await expect(frame.locator("#trash li h5")).toHaveText(["High Tatras 2", "High Tatras 4"])
+
+})

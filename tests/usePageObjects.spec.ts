0e732bac97761b9876d16ba73a5f8f6d17964a26
Gokay Yilmaz <gokay6320@gmail.com>
2026-06-24 14:06:02 +0300

    datepicker page added

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from "@playwright/test"
 import { NavigationPage } from "../page-objects/navigationPage"
 import { FormLayoutsPage } from "../page-objects/formLayoutsPage"
+import { DatePickerPage } from "../page-objects/datePickerPage"
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/")
@@ -18,8 +19,14 @@ test("navigate to form page", async ({ page }) => {
 test("parameterized methods", async ({ page }) => {
   const navigationPage = new NavigationPage(page)
   const formLayoutsPage = new FormLayoutsPage(page)
+  const datePickerPage = new DatePickerPage(page)
 
   await navigationPage.formLayoutsPage()
   await formLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectOption("test@test.com", "Welcome1", "Option 2")
   await formLayoutsPage.submitInlineFormWithNameEmailAndCheckbox("John Smith", "john@test.com", false)
+
+  await navigationPage.datepickerPage()
+  await datePickerPage.selectCommonDatePickerDateFromToday(7)
+  await datePickerPage.selectDatepickerWithRangeFromToday(7, 14)
 })
+

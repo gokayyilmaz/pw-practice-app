6d0814449a5a1548682929e55e6fbcd590d18b55
Gokay Yilmaz <gokay6320@gmail.com>
2026-06-29 14:03:09 +0300

    faker js added for random full name and email generation

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from "@playwright/test"
 import { NavigationPage } from "../page-objects/navigationPage"
 import { FormLayoutsPage } from "../page-objects/formLayoutsPage"
 import { DatePickerPage } from "../page-objects/datePickerPage"
+import { faker } from "@faker-js/faker"
 
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200/")
@@ -20,10 +21,12 @@ test("parameterized methods", async ({ page }) => {
   const navigationPage = new NavigationPage(page)
   const formLayoutsPage = new FormLayoutsPage(page)
   const datePickerPage = new DatePickerPage(page)
+  const randomFullName = faker.person.fullName()
+  const randomEmail = `${randomFullName.replace(/\s+/g, "")}${faker.number.int(1000)}@test.com`
 
   await navigationPage.formLayoutsPage()
   await formLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectOption("test@test.com", "Welcome1", "Option 2")
-  await formLayoutsPage.submitInlineFormWithNameEmailAndCheckbox("John Smith", "john@test.com", false)
+  await formLayoutsPage.submitInlineFormWithNameEmailAndCheckbox(randomFullName, randomEmail, false)
 
   await navigationPage.datepickerPage()
   await datePickerPage.selectCommonDatePickerDateFromToday(7)

4e285bee76e47cbb9b9f23d421f8361a26d2cdcc
Gokay Yilmaz <gokay6320@gmail.com>
2026-06-30 16:42:10 +0300

    Add .env.example for environment variable configuration and update tests to use credentials from environment variables

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -25,7 +25,7 @@ test("parameterized methods", async ({ page }) => {
   const randomEmail = `${randomFullName.replace(/\s+/g, "")}${faker.number.int(1000)}@test.com`
 
   await navigationPage.formLayoutsPage()
-  await formLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectOption("test@test.com", "Welcome1", "Option 2")
+  await formLayoutsPage.submitUsingTheGridFormWithCredentialsAndSelectOption(process.env.USERNAME, process.env.PASSWORD, "Option 2")
   await page.screenshot({path: "screenshots/formsLayoursPage.png"})
   await formLayoutsPage.submitInlineFormWithNameEmailAndCheckbox(randomFullName, randomEmail, false)
   await page.locator("nb-card", { hasText: "Inline form" }).screenshot({path: "screenshots/inlineForm.png"})
